75a900e497b551a3e5917bbb59ca9131502b6c0f
diegooow <62667876+diegooow@users.noreply.github.com>
2023-03-07 10:08:18 -0300

    documentação

--- codigo/produto.java
+++ codigo/produto.java
@@ -1,4 +1,11 @@
-
+/**
+* Esta classe representa um objeto produto com os atributos descricao,preço que foi 
+ * comprado o produto, preço de venda do produto, quantidadede produtis vendida, total arrecado
+ * com a venda, quantidade adquirida(lucro),custo de aquisição, imposto sobre o produto,margem 
+ * de lucro, e quantidade que tem em estoque	
+ * @author 1402031
+ *
+ */
 public class Produto {
 	
 	private String descricao;
@@ -12,6 +19,8 @@ public class Produto {
 	private double margemLucro;
 	private int quantidadeEstoque;
 	
+	/** 
+	 * Cria um novo objeto produto com os dados fornecidos */
 	
 	public Produto(String descricao, double precoCusto, double precoVenda, int quantidadeVendida,
 			double totalArrecadado, int quantidadeAdquirida, double custoAquisicao, double impostos, double margemLucro, int quantidadeEstoque) {
@@ -27,11 +36,19 @@ public class Produto {
 		this.margemLucro = margemLucro;
 		this.quantidadeEstoque = quantidadeEstoque;
 	}
-	
+	/** 
+	 * Retorna a margem de lucro
+	 * @return margem de lucro
+	 */
 	public double getMargemLucro() {
 		return margemLucro;
 	}
 
+	/**
+	 * 
+	 * set da margem de lucro
+	 * @param percentMargemLucro 
+	 */
 	public void setMargemLucro(double percentMargemLucro) {
 		
 		if(30 > margemLucro || margemLucro > 80 ) {
@@ -40,18 +57,42 @@ public class Produto {
 		this.margemLucro = (margemLucro/100) * this.getPrecoCusto()	- this.getPrecoCusto() ;
 	}
 
+	/**
+	 * retorna a decrição do produto
+	 * @return 
+	 */
 	public String getDescricao() {
 		return descricao;
 	}
+	/**
+	 * Set da descrição do produto
+	 * @param descricao
+	 */
 	public void setDescricao(String descricao) {
 		this.descricao = descricao;
 	}
+	
+	/**
+	 * Retorna o preço de custo
+	 * 
+	 * @return o preço de custo
+	 */
 	public double getPrecoCusto() {
 		return precoCusto;
 	}
+	
+	/**
+	 * Set o preço de custo
+	 * @param precoCusto
+	 */
+	
 	public void setPrecoCusto(double precoCusto) {
 		this.precoCusto = precoCusto;
 	}
+	/**
+	 * Retorna o preço de venda
+	 * @return o preço de venda
+	 */
 	public double getPrecoVenda() {
 		return precoVenda;
 	}
@@ -59,52 +100,100 @@ public class Produto {
 	public void setPrecoVenda() {
 		this.precoVenda = this.getPrecoCusto() + this.getPrecoCusto() + this.getImpostos();
 	}
+	/**
+	 * Retorna a quantidade vendida
+	 * @return quatidade vendida
+	 */
 	public int getQuantidadeVendida() {
 		return quantidadeVendida;
 	}
+	/**
+	 * Set da quantidade vendida
+	 * @param quantidadeVendida
+	 */
 	public void setQuantidadeVendida(int quantidadeVendida) {
 		this.quantidadeVendida = quantidadeVendida;
 	}
+	/**
+	 * Retorna o total arrecadado
+	 * @return o total arrecadado
+	 */
 	public double getTotalArrecadado() {
 		return totalArrecadado;
 	}
+	/**
+	 * Set do total arrecadado
+	 * @param totalArrecadado
+	 */
 	public void setTotalArrecadado(double totalArrecadado) {
 		this.totalArrecadado = totalArrecadado;
 	}
+	/**
+	 * quantidade de produtos adquiridos
+	 * @return quantidade adquirida
+	 */
 	public int getQuantidadeAdquirida() {
 		return quantidadeAdquirida;
 	}
+	/**
+	 * Set da quantidade de produtos adquiridos
+	 * @param quantidadeAdquirida
+	 */
 	public void setQuantidadeAdquirida(int quantidadeAdquirida) {
 		this.quantidadeAdquirida = quantidadeAdquirida;
 	}
+	/**
+	 * Retorna custo de aquisição de produtos
+	 * @return o custo de aquisição
+	 */
 	public double getCustoAquisicao() {
 		return custoAquisicao;
 	}
+	/**
+	 * Set do custo de aquisição
+	 * @param custoAquisicao
+	 */
 	public void setCustoAquisicao(double custoAquisicao) {
 		this.custoAquisicao = custoAquisicao;
 	}
+	/**
+	 * Retorna os impostos sobre os produtos
+	 * @return impostos
+	 */
 	public double getImpostos() {
 		return impostos;
 	}
 	
 	// colocar 1.18 uma variavel
+	
+	/**
+	 * set dos impostos sobre os produtos 
+	 */
 	public void setImpostos() {
 		this.impostos = (this.getPrecoCusto() + this.getMargemLucro()) * 1.18;
 	}
 	
+	/**
+	 * Retorna booleano de descrição valida
+	 * @return descrição valida
+	 */
 	 public boolean temDescricaoValida() {
 	        return this.getDescricao().length() >= 3;
 	    }
+	 
+	 /**
+	  * Retorna a quantidade de produtos no estoque
+	  * @return quantidade no estoque
+	  */
 
 	public int getQuantidadeEstoque() {
 		return quantidadeEstoque;
 	}
 
+	/**
+	 * Set da quantidade de produtos no estoque
+	 */
 	public void setQuantidadeEstoque() {
 		this.quantidadeEstoque = this.getQuantidadeAdquirida() - this.getQuantidadeVendida();
 	}
-
-	 
-	 
-	 
 }
