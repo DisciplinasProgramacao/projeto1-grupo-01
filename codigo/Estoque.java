807f9257a71d3af84758b4619a64546a2a53ebab
diegooow <62667876+diegooow@users.noreply.github.com>
2023-03-09 03:51:45 -0300

    Update Estoque.java

--- codigo/Estoque.java
+++ codigo/Estoque.java
@@ -1,1 +1,16 @@
 
+import java.util.ArrayList;
+
+public class Estoque {
+	private int tamanhoMaximo;
+	private ArrayList<Produto> produtos;
+	
+	public Estoque(int tamanhoMaximo) {
+		this.tamanhoMaximo = tamanhoMaximo;
+		this.produtos = new ArrayList<Produto>();
+	}
+	
+	public int getQuantidadeProdutos() {
+        return produtos.size();
+    }
+}
