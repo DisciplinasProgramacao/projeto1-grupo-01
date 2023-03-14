f5c543ed15e01ffad04e80463c64e6049ba7d96b
Tabosaaa <67617287+Tabosaaa@users.noreply.github.com>
2023-03-13 23:06:30 -0300

    Update produto.java

--- codigo/produto.java
+++ codigo/produto.java
@@ -13,6 +13,8 @@ import java.util.Scanner;
 
 public class Produto {
 	
+	
+	//#region ATRIBUTOS
 	private String descricao;
 	private double precoCusto;
 	private double precoVenda;
@@ -24,9 +26,12 @@ public class Produto {
 	private double margemLucro;
 	private int quantidadeEstoque;
 	private int quantidadeMinima;
+	//#endregion
+	
 	
 	/** 
-	 * Cria um novo objeto produto com os dados fornecidos */
+	 * Cria um novo objeto produto com os dados fornecidos 
+	 */
 	
 	public Produto() {
 		super();
@@ -43,16 +48,16 @@ public class Produto {
 	}
 	/** 
 	 * Retorna a margem de lucro
-	 * @return margem de lucro
+	 * @return margemlucro Representa a margem de lucro do produto
 	 */
 	public double getMargemLucro() {
 		return margemLucro;
 	}
 
 	/**
-	 * 
-	 * set da margem de lucro
-	 * @param percentMargemLucro 
+	 * Set da margem de lucro.
+	 * A margem de lucro deve ser sempre maior que 30 e menor que 80.
+	 * @param percentMargemLucro Representa a percentagem da margem de lucro
 	 */
 	public void setMargemLucro(double percentMargemLucro) {
 		if(30 > percentMargemLucro || percentMargemLucro > 80 ) {
@@ -63,15 +68,16 @@ public class Produto {
 	}
 
 	/**
-	 * retorna a decrição do produto
-	 * @return 
+	 * Retorna a decrição do produto
+	 * @return descricao Representa a descrição do produto
 	 */
 	public String getDescricao() {
 		return descricao;
 	}
 	/**
-	 * Set da descrição do produto
-	 * @param descricao
+	 * Set da descrição do produto.
+	 * A descrição deve ter mais que três caracteres
+	 * @param descricao Representa a descrição que será inserida no produto
 	 */
 	public void setDescricao(String descricao) {
     		if(descricao.length() <= 3) {
@@ -84,99 +90,101 @@ public class Produto {
 	
 	/**
 	 * Retorna o preço de custo
-	 * 
-	 * @return o preço de custo
+	 * @return precoCusto Representa o preço de custo
 	 */
 	public double getPrecoCusto() {
 		return precoCusto;
 	}
 	
 	/**
-	 * Set o preço de custo
-	 * @param precoCusto
+	 * Método set do preço de custo
+	 * @param precoCusto Representa o preço de custo do produto
 	 */
 	
 	public void setPrecoCusto(double precoCusto) {
 		this.precoCusto = precoCusto;
 	}
 	/**
-	 * Retorna o preço de venda
-	 * @return o preço de venda
+	 * Método get do preco de venda
+	 * @return precoVenda Representa o preço de venda do produto
 	 */
 	public double getPrecoVenda() {
 		return precoVenda;
 	}
-	//mudar precocusto
+	
+	
+	/**
+	 * Método set do preco de venda, utilizando o preço do custo mais a margem de lucro mais o imposto para calcular o preço da venda
+	 */
 	public void setPrecoVenda() {
 	    this.precoVenda = this.getPrecoCusto() + this.getMargemLucro() + this.getImpostos();
 	}
 
 	/**
-	 * Retorna a quantidade vendida
-	 * @return quatidade vendida
+	 * Retorna a quantidade vendida do produto
+	 * @return quantidadeVendida Representa a quantidade vendida pelo produto
 	 */
 	public int getQuantidadeVendida() {
 		return quantidadeVendida;
 	}
 	/**
 	 * Set da quantidade vendida
-	 * @param quantidadeVendida
+	 * @param quantidadeVendida Representa a quantidade de produtos vendidos
 	 */
 	public void setQuantidadeVendida(int quantidadeVendida) {
 		this.quantidadeVendida = quantidadeVendida;
 	}
 	/**
 	 * Retorna o total arrecadado
-	 * @return o total arrecadado
+	 * @return totalArrecadado Representa o total arrecadado
 	 */
 	public double getTotalArrecadado() {
 		return totalArrecadado;
 	}
 	/**
-	 * Set do total arrecadado
-	 * @param totalArrecadado
+	 * Set do total arrecadado, sendo esse calculado pela subtração do preço de venda vezes a quantidade vendida menos o preço de custo vezes a quantidades adiquiridas 
 	 */
-	public void setTotalArrecadado(double totalArrecadado) {
-		this.totalArrecadado = totalArrecadado;
+	public void setTotalArrecadado() {
+		this.totalArrecadado = this.getPrecoVenda() * this.getQuantidadeVendida() - this.getQuantidadeAdquirida() * this.getCustoAquisicao();
 	}
 	/**
 	 * quantidade de produtos adquiridos
-	 * @return quantidade adquirida
+	 * @return quantidadeAdquirida Representa a quantidade de produtos adiquirida
 	 */
 	public int getQuantidadeAdquirida() {
 		return quantidadeAdquirida;
 	}
 	/**
 	 * Set da quantidade de produtos adquiridos
-	 * @param quantidadeAdquirida
+	 * @param quantidadeAdquirida Representa a quantidade de produtos adiquirida
 	 */
 	public void setQuantidadeAdquirida(int quantidadeAdquirida) {
 		this.quantidadeAdquirida = quantidadeAdquirida;
 	}
 	/**
 	 * Retorna custo de aquisição de produtos
-	 * @return o custo de aquisição
+	 * @return custoAquisicao Representa o custo de aquisição de um produto
 	 */
 	public double getCustoAquisicao() {
 		return custoAquisicao;
 	}
 	/**
 	 * Set do custo de aquisição
-	 * @param custoAquisicao
+	 * @param custoAquisicao Representa o custo de aquisição de um produto
 	 */
 	public void setCustoAquisicao(double custoAquisicao) {
 		this.custoAquisicao = custoAquisicao;
 	}
 	/**
 	 * Retorna os impostos sobre os produtos
-	 * @return impostos
+	 * @return impostos Representa os impostos de um produto
 	 */
 	public double getImpostos() {
 		return impostos;
 	}
 	
 	/**
-	 * set dos impostos sobre os produtos 
+	 * set dos impostos sobre os produtos, sendo calculado pela soma do preço de custo mais a margem de lucro multiplicados por 1.18 subtraidos pelo preço de custo + margem de lucro
 	 */
 	public void setImpostos() {
 		this.impostos = (this.getPrecoCusto() + this.getMargemLucro()) * 1.18 - (this.getPrecoCusto() + this.getMargemLucro());
@@ -185,29 +193,38 @@ public class Produto {
 	 
 	 /**
 	  * Retorna a quantidade de produtos no estoque
-	  * @return quantidade no estoque
+	  * @return quantidadeEstoque Representa a quantidade desse produtos no stoque
 	  */
 
 	public int getQuantidadeEstoque() {
 		return quantidadeEstoque;
 	}
 	
-	public boolean monitorarEstoque(){
-		Scanner scanner = new Scanner(System.in);
-		System.out.println("Digite em unidades o estoque mínimo desse produto");
-		int n = scanner.nextInt();
-		if(n > this.getQuantidadeEstoque()) {
-			System.out.println("O produto tem falta no estoque, existindo " + this.getQuantidadeEstoque() + "unidades desse produto no estoque." );
-			return true;
-		}else {
-			System.out.println("Não está há falta desse produto no estoque, existindo " + this.getQuantidadeEstoque() + "unidades desse produto no estoque.");
-			return false;
-		}
-	}
+	
+	/**
+	 * Monitora o estoque desse produto para verificar se ele possuí a quantidade mínima de unidades no estoque
+	 * @param unidadesMinimas Representa as unidades mínimas que esse produto deve ter no estoque 
+	 * @return true Caso o produto tenha falta no estoque 
+	 * @return false Caso o produto não tenha falta no estoque
+	 */
+	
+	public boolean monitorarEstoque(int unidadesMinimas){
+
+        if(unidadesMinimas > this.getQuantidadeEstoque()) {
+            System.out.println("O produto tem falta no estoque, existindo " + this.getQuantidadeEstoque() + "unidades desse produto no estoque." );
+            return true;
+        }else {
+            System.out.println("Não está há falta desse produto no estoque, existindo " + this.getQuantidadeEstoque() + "unidades desse produto no estoque.");
+            return false;
+        }
+    }
 
 	/**
 	 * Set da quantidade de produtos no estoque
+	 * @param quantidadeAdquirida Representa a quantidade adiquirida do produto no estoque
+	 * @param quantidadeVendida Representa a quantidade vendida do produto no estoque
 	 */
+	
 	public void setQuantidadeEstoque(int quantidadeAdquirida,int quantidadeVendida) {
 		this.quantidadeAdquirida = quantidadeAdquirida;
 	    this.quantidadeVendida = quantidadeVendida;
